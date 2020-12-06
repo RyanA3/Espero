61958d71657a7fc3622a69867685149a7b254b1a
RyanA3 <alsobrooks.ryan@gmail.com>
2020-12-06 12:34:10 -0500

    Make Command Help Dynamic

--- Espero/src/me/felnstaren/espero/module/nations/command/nation/NationCommandMaster.java
+++ Espero/src/me/felnstaren/espero/module/nations/command/nation/NationCommandMaster.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 
 import me.felnstaren.espero.command.CommandStub;
 import me.felnstaren.espero.command.MasterCommand;
+import me.felnstaren.espero.config.EsperoPlayer;
 import me.felnstaren.espero.module.nations.command.nation.create.NationCreateSub;
 import me.felnstaren.espero.module.nations.command.nation.demote.NationDemoteSub;
 import me.felnstaren.espero.module.nations.command.nation.invite.NationInviteSub;
@@ -13,6 +14,8 @@ import me.felnstaren.espero.module.nations.command.nation.kick.NationKickSub;
 import me.felnstaren.espero.module.nations.command.nation.leader.NationLeaderSub;
 import me.felnstaren.espero.module.nations.command.nation.leave.NationLeaveSub;
 import me.felnstaren.espero.module.nations.command.nation.promote.NationPromoteSub;
+import me.felnstaren.espero.module.nations.nation.Nation;
+import me.felnstaren.espero.module.nations.nation.NationPlayerRank;
 import me.felnstaren.espero.util.message.Messenger;
 import me.felnstaren.espero.util.message.json.Message;
 import me.felnstaren.espero.util.message.json.TextComponent;
@@ -22,15 +25,37 @@ public class NationCommandMaster extends MasterCommand {
 	public NationCommandMaster() {
 		super(new CommandStub() {
 			public boolean handle(CommandSender sender, String[] args, int current) {
+				Player player = (Player) sender;
+				EsperoPlayer eplayer = new EsperoPlayer(player);
+				Nation nation = eplayer.getNation();
+				
 				Message message = new Message();
 				message.addComponent(new TextComponent("-= Nations =-").setColor("#0077FF"));
 				message.addComponent(new TextComponent("   by Felns").setColor("#CCCCCC").setItalic(true));
-				message.addComponent(new TextComponent("\n /nation create <name>").setColor("#AAAAAA"));
-				message.addComponent(new TextComponent("\n /nation join <name>").setColor("#AAAAAA"));
-				message.addComponent(new TextComponent("\n /nation leave").setColor("#AAAAAA"));
-				message.addComponent(new TextComponent("\n /nation claim <id>").setColor("#AAAAAA"));
+				message.addComponent(new TextComponent("\n /nation").setColor("#AAAAAA"));
 				
-				Messenger.send((Player) sender, message);
+				if(nation == null) {
+					message.addComponent(new TextComponent("\n         create <name>").setColor("#AAAAAA"));
+					message.addComponent(new TextComponent("\n         join <nation>").setColor("#AAAAAA"));
+				} else {
+					message.addComponent(new TextComponent("\n         leave").setColor("#AAAAAA"));
+					
+					NationPlayerRank rank = eplayer.getNationRank();
+					if(rank.isPermitted("recruit")) 
+						message.addComponent(new TextComponent("\n         invite <player>").setColor("#AAAAAA"));
+					if(rank.isPermitted("kick"))
+						message.addComponent(new TextComponent("\n         kick <player>").setColor("#AAAAAA"));
+					if(rank.isPermitted("promote"))
+						message.addComponent(new TextComponent("\n         promote <player>").setColor("#AAAAAA"));
+					if(rank.isPermitted("demote"))
+						message.addComponent(new TextComponent("\n         demote <player>").setColor("#AAAAAA"));
+					if(rank.isPermitted("claim"))
+						message.addComponent(new TextComponent("\n         claim").setColor("#AAAAAA"));
+					if(nation.getNextHighestRank(rank) == null)
+						message.addComponent(new TextComponent("\n         leader <player>").setColor("#AAAAAA"));
+				}
+				
+				Messenger.send(player, message);
 				return true;
 			}
 		}, "nation", "espero.nation");
